4d25abdf02ba70d73c424c8af0c0101b48c41089
Danielmg05 <92178121+Danielmg05@users.noreply.github.com>
2024-11-29 19:26:04 +0100

    Añadido código para cambiar de pantallas desde main

---
+++ cliente/src/main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# Escena que está cargada siempre, independientemente de si el jugador está en el menú o jugando
+# El script instancia en current_screen la pantalla en la uqe está el juego en cada momento
+
+var menu_scene = preload("res://menu/main.tscn")
+var modal_window = preload("res://modal_window.tscn")
+
+func _ready() -> void:
+	change_screen(menu_scene)
+
+
+func change_screen(scene):
+	remove_child(get_node("res:/current_screen"))
+	var s = scene.instantiate()
+	s.name = "current_screen"
+	add_child(s)
