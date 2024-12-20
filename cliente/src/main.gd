836dbd5a0aa090993a616299c88f3c0bb31fff44
localanomaly <92178121+Danielmg05@users.noreply.github.com>
2024-12-20 19:11:35 +0100

    Terminada ventana modal

--- cliente/src/main.gd
+++ cliente/src/main.gd
@@ -3,13 +3,11 @@ extends Node2D
 # Escena que está cargada siempre, independientemente de si el jugador está en el menú o jugando
 # El script instancia en current_screen la pantalla en la uqe está el juego en cada momento
 
-var closing_function = hide_modal_window()
 var menu_scene = preload("res://screens/menu/main.tscn")
 var modal_window = preload("res://modal_window.tscn")
 
 func _ready() -> void:
 	change_screen(menu_scene)
-
 
 
 func change_screen(scene):
@@ -19,18 +17,14 @@ func change_screen(scene):
 	add_child(s)
 
 
-func show_modal_window(text,button_text="Ok"):
+func show_modal_window(text: String, on_close: Callable = func(): pass, button_text: String = "Ok"):
 	var m = modal_window.instantiate()
 	m.name = "modal_window"
 	m.get_node("Label").text = text
 	m.get_node("Button").text = button_text
+	m._on_close = on_close
 	get_tree().paused = true
 	add_child(m)
-
-
-func _on_modal_window_closed():
-	#closing_function
-	hide_modal_window()
 
 
 func hide_modal_window():
