4c1a256d9ec80141385bc9137eb2f87ae56ed657
Danielmg05 <92178121+Danielmg05@users.noreply.github.com>
2025-01-10 19:31:05 +0100

    Añadido fade y transición entre pantallas

--- cliente/src/screens/menu/main.gd
+++ cliente/src/screens/menu/main.gd
@@ -5,14 +5,14 @@ var identification = preload("res://screens/menu/identification.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	change_screen(identification)
+	change_window(identification)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func change_screen(scene):
+func change_window(scene):
 	remove_child(get_node("current_screen"))
 	var s = scene.instantiate()
 	s.name = "current_screen"
