5703909a08a77741b89f86f572b937449b28322c
asexto <a.sexto@gmail.com>
2025-01-17 19:41:27 +0100

    El servidor permite conexiones básicas

--- server/main.js
+++ server/main.js
@@ -1,19 +1,19 @@
 const WebSocket = require('ws');
-
+const { v4: uuidv4 } = require('uuid');
 const MAX_PEERS = 100;
-const MAX_LOBBIES = 5;
+//const MAX_LOBBIES = 5;
 const PORT = 9090;
 
 const wss = new WebSocket.Server({ port: PORT });
-const clients = new Map();
+const peers = new Map();
 
-class Client {
+class Peer {
     constructor(id, ws) {
         this.id = id;
         this.ws = ws;
         /*
         this.lobby = '';
-        // Close connection after 1 sec if client has not joined a lobby
+        // Close connection after 1 second if client has not joined a lobby
         this.timeout = setTimeout(() => {
             if (!this.lobby) {
                 ws.close(4000, "Have not joined lobby yet");
@@ -24,25 +24,23 @@ class Client {
 }
 
 wss.on('connection', function connection(ws) {
-    if (clients.size >= MAX_PEERS) {
+    if (peers.size >= MAX_PEERS) {
         ws.close(4000, "Too many peers connected");
+        console.log('Connection refused (too many peeers connected)');
         return;
     }
     const id = uuidv4();
-    const client = new Peer(id, ws);
-    clients.set(client);
-    
+    const peer = new Peer(id, ws);
+    peers.set(peer);
+    console.log('Peer connected (id: %s)', peer.id);
     ws.on('message', function message(data) {
         console.log('received: %s', data);
+        ws.send("Pong! (test answer)");
     });
 
     ws.on("close", function close(code, data) {
-        const reason = data.toString();
-        peersCount--;
-        console.log(`Client ${client.id} closed `
-            + `with reason ${code}: ${reason}`);
-        clients.delete(client);
-        console.log('client disconected');
+        console.log(`Peer disconnected (id: ${peer.id}, code: ${code}, reason: ${data.toString()})`);
+        peers.delete(peer);
     });
 
     //ws.send('something');
