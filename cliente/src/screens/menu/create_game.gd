1e1c8beae0fdb00c9319a782e43cc0dc173788db
Happy-reindeer007 <alexfreirefdez@gmail.com>
2025-01-31 19:38:49 +0100

    Progreso en create game

--- cliente/src/screens/menu/create_game.gd
+++ cliente/src/screens/menu/create_game.gd
@@ -2,20 +2,24 @@ extends Control
 
 var gamemodes = ["Clásico", "Supervivencia", "Infectado"]
 var gamemodes_index = 0
-var gamemode_description = ["Cuando te pillan, el jugador que te pilló deja de \n
-pillar, ahora pillas tú", "Si te pillan estás eliminado, gana el último que \n
-quede vivo", "Si te pillan, ahora pillas tú y todos los que estuvieran pillados \n
-antes"]
 
+var clasico_descripcion = "Si te pillan, el jugador que te pilló deja de " \
++ "pillar, ahora pillas tú. Pierde el que está pillando cuando se acaba el tiempo."
+var supervivencia_descripcion = "Si te pillan estás eliminado. Cuando se " \
++ "acaba el tiempo, ganan todos los que escapan que quedan vivos o, en el caso " \
++ "de que no quede ninguno, el que pilla."
+var infectado_descripcion = "Si te pillan, pillas tú con todos los que " \
++ "estuvieran pillados antes."
+var gamemode_description = [clasico_descripcion, supervivencia_descripcion, \
+infectado_descripcion]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$CreateGame.visible = true
 	$ChooseMap.visible = false
-	$Gamemode.text = gamemodes[gamemodes_index]
-	$CreateGame/GamemodeDescriptionTxt.text = "Cuando te pillan, el jugador que \n  
-	te pilló deja de pillar, ahora pillas tú"
-	$Gamemode.visible = true
+	$CreateGame/GamemodeNameTxt.text = gamemodes[gamemodes_index]
+	$CreateGame/GamemodeDescriptionTxt.text = clasico_descripcion
+	$CreateGame/GamemodeNameTxt.text = gamemodes[gamemodes_index]
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,23 +27,34 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_button_next_pressed() -> void:
-	$CreateGame.visible = false
-	$ChooseMap.visible = true
-	$Gamemode.visible = false
-	
+	if $CreateGame.visible:
+		$CreateGame.visible = false
+		$ChooseMap.visible = true
 
 
 func _on_button_go_back_pressed() -> void:
-	$CreateGame.visible = true
-	$ChooseMap.visible = false
-	$Gamemode.visible = true
+	if $ChooseMap.visible:
+		$CreateGame.visible = true
+		$ChooseMap.visible = false
 
 
-func _on_gamemode_pressed() -> void:
 
-	if gamemodes_index == 2:
+	var tumama = """if gamemodes_index == 2:
 		gamemodes_index = 0
 	else:
 		gamemodes_index += 1
 	$Gamemode.text = gamemodes[gamemodes_index]
-	$CreateGame/GamemodeDescriptionTxt.text = gamemode_description[gamemodes_index]
+	$CreateGame/GamemodeDescriptionTxt.text = gamemode_description[gamemodes_index]"""
+
+
+func _on_left_arrow_txt_pressed() -> void:
+	if gamemodes_index != 0:
+		gamemodes_index -= 1
+		$CreateGame/GamemodeNameTxt.text = gamemodes[gamemodes_index]
+		$CreateGame/GamemodeDescriptionTxt.text = gamemode_description[gamemodes_index]
+
+func _on_right_arrow_txt_pressed() -> void:
+	if gamemodes_index != 2:
+			gamemodes_index += 1
+			$CreateGame/GamemodeNameTxt.text = gamemodes[gamemodes_index]
+			$CreateGame/GamemodeDescriptionTxt.text = gamemode_description[gamemodes_index]
