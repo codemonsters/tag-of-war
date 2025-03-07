d65c0067afb66958e89fb64fd5c8cfb277e71f5a
Carloosrs <carlosrsoto10@gmail.com>
2025-03-07 18:35:22 +0100

    Arreglos con el verbo "receive"

--- cliente/src/screens/menu/menu_main.gd
+++ cliente/src/screens/menu/menu_main.gd
@@ -4,7 +4,7 @@ var identification = preload("res://screens/menu/identification.tscn")
 var server_list = preload("res://screens/menu/sala_espera_admin.tscn")
 var create_account = preload("res://screens/menu/create_account.tscn")
 var recuperar_contraseña = preload("res://screens/menu/recuperar_contraseña.tscn")
-var lista_servidores = preload("res://screens/menu/lista_servidores.tscn")
+var lista_servidores = preload("res://screens/menu/server_list/server_list.tscn")
 var utilities = Utilities.new()
 
 signal screen_connect_to_server()
