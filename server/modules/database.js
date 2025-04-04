aff0f7e08a5c394a1696722d8f84cd49d56c6ff1
asexto <a.sexto@gmail.com>
2025-04-04 13:04:29 +0200

    Refactoring general
    Reorganizado el código en distintos objetos (WIP)

--- server/modules/database.js
+++ server/modules/database.js
@@ -1,24 +1,28 @@
-const SQLITE_FILENAME = "database.sqlite"
+const database = new Object();
 
-const fs = require('fs');
-const BetterSqlite3 = require('better-sqlite3');
+database.SQLITE_FILENAME = "database.sqlite";
+database.fsModule = require('fs');
+database.BetterSqlite3Module = require('better-sqlite3');
+database.init = function() {
+    // initialize database
+    let create_db_schema;
+    if (this.fsModule.existsSync(this.SQLITE_FILENAME)) {
+        create_db_schema = false;
+    } else {
+        create_db_schema = true;
+    }
 
-// initialize database
-if (fs.existsSync(SQLITE_FILENAME)) {
-    create_db_schema = false;
-} else {
-    create_db_schema = true;
+    this.sqlite = new this.BetterSqlite3Module(this.SQLITE_FILENAME);
+    if (create_db_schema) {
+        console.debug("Creando base de datos...")
+        // creamos las tablas de la base de datos
+        this.sqlite.prepare("CREATE TABLE active_players (player_id INTEGER PRIMARY KEY)").run();
+        this.sqlite.prepare("CREATE TABLE profiles (player_id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(20) NOT NULL UNIQUE, email VARCHAR(30) UNIQUE, password VARCHAR(32), last_connection_timestamp DATETIME NOT NULL, account_creation_timestamp DATETIME, account_verified BOOLEAN)").run();
+        this.sqlite.prepare("CREATE TABLE rooms (room_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32) NOT NULL UNIQUE, admin_player_id INTEGER NOT NULL UNIQUE)").run();
+    }
+    this.sqlite.prepare("DELETE FROM active_players").run();
+    this.sqlite.prepare("DELETE FROM profiles WHERE email IS NULL").run();
+    this.sqlite.prepare("DELETE FROM rooms").run();
 }
 
-const db = new BetterSqlite3(SQLITE_FILENAME);
-if (create_db_schema) {
-    // creamos las tablas de la base de datos
-    db.prepare("CREATE TABLE active_players (player_id INTEGER PRIMARY KEY)").run();
-    db.prepare("CREATE TABLE profiles (player_id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(20) NOT NULL UNIQUE, email VARCHAR(30) UNIQUE, password VARCHAR(32), last_connection_timestamp DATETIME NOT NULL, account_creation_timestamp DATETIME, account_verified BOOLEAN)").run();
-    db.prepare("CREATE TABLE rooms (room_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32) NOT NULL UNIQUE, admin_player_id INTEGER NOT NULL UNIQUE)").run();
-}
-db.prepare("DELETE FROM active_players").run();
-db.prepare("DELETE FROM profiles WHERE email IS NULL").run();
-db.prepare("DELETE FROM rooms").run();
-
-module.exports = { db }
+module.exports = { database }
