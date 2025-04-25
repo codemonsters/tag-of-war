fce2cf5f1bd48c2b06221974ea9942f06db372d9
yagueto <yagofreirefdez@gmail.com>
2025-04-25 19:36:27 +0200

    Elimina errores al cambiar de ventanas

--- cliente/src/screens/menu/main.gd
+++ cliente/src/screens/menu/main.gd
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	pass
 
 func change_window(scene):
-	remove_child(get_node("current_screen"))
+	if get_node_or_null("current_screen") != null: remove_child(get_node("current_screen"))
 	var s = scene.instantiate()
 	s.name = "current_screen"
 	add_child(s)
