abb926c6a983abc37c1f9d2b4f730256d510c15d
xJuanchoux <147860507+xJuanchoux@users.noreply.github.com>
2025-05-30 18:46:16 +0200

    Optimizado el movimiento de personaje y juego de prueba

--- cliente/src/screens/game/player.gd
+++ cliente/src/screens/game/player.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 @export var walkspeed = 320
 ##Pixels per second
-@export var acceleration = 640
+@export var acceleration = 800
 ##Pixels per second
 @export var deceleration = 640
 
-@export var jump = 250
-@export var gravity = 5
-@export var terminal_velocity = 400
+@export var jump = 500
+@export var gravity = 20
+@export var terminal_velocity = 600
 
 var speed = 0
 var walk_direction = 1
@@ -21,17 +21,21 @@ func _physics_process(delta: float) -> void:
 	var on_floor = is_on_floor()
 	
 	if Input.is_action_pressed("walk_right"):
-		walk_direction = 1
+		if walk_direction == -1:
+			speed = 0
+			walk_direction = 1
 		speed = clamp(speed + (acceleration * delta), 0, walkspeed)
 	elif Input.is_action_pressed("walk_left"):
-		walk_direction = -1
+		if walk_direction == 1:
+			speed = 0
+			walk_direction = -1
 		speed = clamp(speed + (acceleration * delta), 0, walkspeed)
 	else:
 		speed = clamp(speed - (deceleration * delta), 0, walkspeed)
 	
-	if Input.is_action_pressed("jump") and on_floor:
+	if Input.is_action_pressed("jump") and on_floor and velocity.y == 0:
 		velocity.y -= jump
-	
+		
 	velocity.x = speed * walk_direction
 	
 	if not on_floor:
