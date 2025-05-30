5c6d6157d57d6b576ddd18e68299a7a56881f782
xJuanchoux <147860507+xJuanchoux@users.noreply.github.com>
2025-05-30 19:21:04 +0200

    creado el boceto del walljump

--- cliente/src/screens/game/player.gd
+++ cliente/src/screens/game/player.gd
@@ -12,13 +12,19 @@ extends CharacterBody2D
 
 var speed = 0
 var walk_direction = 1
+var walljump_available = null
 
 func _ready() -> void:
 	velocity.y += 100
 
 func _physics_process(delta: float) -> void:
 	
+	if is_on_floor():
+		walljump_available = true
+	
 	var on_floor = is_on_floor()
+	
+	var on_wall = is_on_wall()
 	
 	if Input.is_action_pressed("walk_right"):
 		if walk_direction == -1:
@@ -33,9 +39,13 @@ func _physics_process(delta: float) -> void:
 	else:
 		speed = clamp(speed - (deceleration * delta), 0, walkspeed)
 	
-	if Input.is_action_pressed("jump") and on_floor and velocity.y == 0:
-		velocity.y -= jump
-		
+	if Input.is_action_pressed("jump"):
+		if on_floor and velocity.y == 0:
+			velocity.y -= jump
+		elif on_wall and walljump_available == true:
+			velocity.y -= jump
+			walljump_available = false
+			
 	velocity.x = speed * walk_direction
 	
 	if not on_floor:
